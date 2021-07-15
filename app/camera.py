7bbf10b6b5e03464b43a8fd6c89b2c50a9b1eebe
Seung Jae Yang <sjyang1126@gmail.com>
2021-07-15 18:29:55 -0400

    Cleaned up code and restructured file folders

--- app/camera.py
+++ app/camera.py
@@ -12,21 +12,19 @@ class VideoCamera(object):
         self.stream.stop()
 
     def get_frame(self, model):
-        image = self.stream.read()
-        #time.sleep(1)
-        #image = cv2.imread('../happyasian2.jpeg')
+        image = self.stream.read() # Read stream
 
-        detector = cv2.CascadeClassifier('../haarcascade_frontalface_default.xml')
+        detector = cv2.CascadeClassifier('../haarcascade_frontalface_default.xml') # Load face detection model from OpenCV
         face = detector.detectMultiScale(image, 1.1, 4)
         gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
         face_roi = None
 
+        ### Detects all faces within the image frame
         for (x,y,w,h) in face:
             roi_gray = gray[y:y+h, x:x+w]
             roi_color = image[y:y+h, x:x+w]
 
-            cv2.rectangle(image,(x,y), (x+w, y+h), SPOTIFY_GREEN, 2)
-            
+            cv2.rectangle(image,(x,y), (x+w, y+h), SPOTIFY_GREEN, 2) # Draw rectangle arounf detected face
 
             faces = detector.detectMultiScale(roi_gray)
             if len(faces) == 0:
@@ -35,19 +33,21 @@ class VideoCamera(object):
                 for (ex,ey,ew,eh) in faces:
                     face_roi = roi_color[ey:ey+eh, ex:ex+ew]
 
+
         if face_roi is not None:            
             final_image = cv2.resize(face_roi, (224,224))
             final_image = np.expand_dims(final_image, axis=0)
-            final_image = final_image/255.0
+            final_image = final_image/255.0 # Data Normalization
             
         else:
             final_image = None
 
+
         if final_image is not None:
-            Predictions = model.model.predict(final_image)
+            Predictions = model.model.predict(final_image) # Returns predictions - emotions indexed by integer
             label_to_text = {0:'anger', 1:'disgust', 2:'fear', 3:'happiness', 4: 'sadness', 5: 'surprise', 6: 'neutral'} 
-            prediction = label_to_text[np.argmax(Predictions)]
-            label_pos = (x, y - 10)     
+            prediction = label_to_text[np.argmax(Predictions)] # Max value is the closest estimate to the actual emotion
+            label_pos = (x, y - 10) # Position of emotion text is right above the box
             cv2.putText(image, prediction, (x, y-10), cv2.FONT_HERSHEY_SIMPLEX, 1, SPOTIFY_GREEN, 2)
         else:
             cv2.putText(image, "Face Not Detected", (30, 80), cv2.FONT_HERSHEY_SIMPLEX, 1, SPOTIFY_GREEN, 2)
